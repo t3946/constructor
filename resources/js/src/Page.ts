7ecdee2f9490767695f174145773dd86a009efbe
Vladislav <svialence@yandex.ru>
2023-10-08 14:37:04 +0300

    generate unique keys for elements

--- resources/js/src/Page.ts
+++ resources/js/src/Page.ts
@@ -2,6 +2,35 @@ import {Cash} from "cash-dom";
 
 export class Page {
   private readonly $page: Cash;
+  private nextElementIdNumber = 1;
+
+  constructor(elem) {
+    this.$page = $(elem);
+
+    this.addIds(this.$page);
+  }
+
+  public generateKey() {
+    const key = 'element_' + this.nextElementIdNumber;
+
+    this.nextElementIdNumber += 1;
+
+    return key;
+  }
+
+  public addIds($source, level = 0) {
+    for (const elem of $source.children()) {
+      const $elem = $(elem);
+
+      if (!$elem.attr('data-key')) {
+        $elem.attr('data-key', this.generateKey());
+      }
+
+      if ($elem.children()) {
+        this.addIds($elem, level + 1);
+      }
+    }
+  }
 
   public addNewElement(tagName: string) {
     const $newElement = $(`<${tagName}>`);
@@ -12,10 +41,7 @@ export class Page {
     const event = new CustomEvent('domChange');
 
     this.$page[0].dispatchEvent(event);
-  }
-
-  constructor(elem) {
-    this.$page = $(elem);
+    this.addIds(this.$page);
   }
 
   public getRoot() {
