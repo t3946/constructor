7d7b64a19962f0555801082f00e24208794affbd
Vladislav <svialence@yandex.ru>
2023-10-08 09:57:30 +0300

    print new button menu

--- public/index.php
+++ public/index.php
@@ -8,27 +8,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="/dist/app.css?<?= $assetsHash ?>" rel="stylesheet" />
+    <link href="/dist/app.css?<?= $assetsHash ?>" rel="stylesheet"/>
     <script src="/dist/app.js?<?= $assetsHash ?>"></script>
 </head>
 <body>
-    <div class="app">
-        <div class="window tools">
-            <h2>Tools</h2>
-            <ul>
-                <li>&lt;BUTTON&gt;Button</li>
-                <li>&lt;P&gt;Paragraph</li>
-                <li>&lt;DIV&gt;Div</li>
-            </ul>
-        </div>
+<div class="app">
+    <div class="window tools">
+        <h2>Tools</h2>
+        <?php
+        $elements = [
+            'p',
+            'div',
+            'h1',
+            'h2',
+            'h3',
+            'img',
+            'form',
+            'button',
+        ]
+        ?>
+        <ul class="list">
+            <?php foreach ($elements as $elem): ?>
+                <li class="item"><?= '&lt' . strtoupper($elem) . '&gt' ?></li>
+            <?php endforeach ?>
+        </ul>
+    </div>
 
-        <div class="workingArea page">
-            <h1>Моя новая <b>страница</b></h1>
-        </div>
+    <div class="workingArea page">
+        <h1>Моя новая <b>страница</b></h1>
+    </div>
 
-        <div class="window elements">
-            <h2>Elements</h2>
-            <div class="elementsList"></div>
+    <div class="window elements">
+        <h2>Elements</h2>
+        <div class="elementsList"></div>
         </div>
     </div>
 </body>
